d3af266596b28290fdddd03d4c4205f2afbe070f
Vivek Tiwary <Vivek.Tiwary@Honeywell.com>
2021-06-27 10:48:21 +0530

    mysql integration wip

--- source/config/config.ts
+++ source/config/config.ts
@@ -2,6 +2,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || '<DBNAME>';
+const MYSQL_USER = process.env.MYSQL_USER || 'superuser';
+const MYSQL_PASS = process.env.MYSQL_PASS || 'password';
+
+const MYSQL = {
+    host: MYSQL_HOST,
+    database: MYSQL_DATABASE,
+    user: MYSQL_USER,
+    pass: MYSQL_PASS
+};
+
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
@@ -12,6 +25,7 @@ const SERVER = {
 };
 
 const config = {
+    mysql: MYSQL,
     server: SERVER
 };
 
