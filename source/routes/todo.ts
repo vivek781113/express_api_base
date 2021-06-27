502e4fe203f324f18fe49610a42f790784409b44
Vivek Tiwary <Vivek.Tiwary@Honeywell.com>
2021-06-27 14:51:26 +0530

    validator should be called eagerly

--- source/routes/todo.ts
+++ source/routes/todo.ts
@@ -5,7 +5,7 @@ import TodoValidator from '../validator/todo';
 
 const router = express.Router();
 
-router.post('/create', TodoValidator.checkCreateTodo, Middleware.handleValidationError, TodoController.create);
-router.get('/read', TodoValidator.checkReadTodo, Middleware.handleValidationError, TodoController.read);
+router.post('/create', TodoValidator.checkCreateTodo(), Middleware.handleValidationError, TodoController.create);
+router.get('/read', TodoValidator.checkReadTodo(), Middleware.handleValidationError, TodoController.read);
 
 export default router;
