da1c702ef0dd7485873abfd563b36721a11fbad4
Vivek Tiwary <Vivek.Tiwary@Honeywell.com>
2021-07-11 17:35:16 +0530

    order creation successful

--- source/controllers/shiprocket-controller.ts
+++ source/controllers/shiprocket-controller.ts
@@ -7,56 +7,6 @@ import ShiprocketApi from '../services/shiprocket-api';
 
 const NAMESPACE = 'ShiprocketController';
 
-const orderPayload: OrderPayload = {
-    "order_id": "224-477",
-    "order_date": "2021-01-24 11:11",
-    "pickup_location": "Jammu",
-    "channel_id": "12345",
-    "comment": "Reseller: M/s Goku",
-    "billing_customer_name": "Naruto",
-    "billing_last_name": "Uzumaki",
-    "billing_address": "House 221B, Leaf Village",
-    "billing_address_2": "Near Hokage House",
-    "billing_city": "New Delhi",
-    "billing_pincode": "110002",
-    "billing_state": "Delhi",
-    "billing_country": "India",
-    "billing_email": "naruto@uzumaki.com",
-    "billing_phone": "9876543210",
-    "shipping_is_billing": true,
-    "shipping_customer_name": "",
-    "shipping_last_name": "",
-    "shipping_address": "",
-    "shipping_address_2": "",
-    "shipping_city": "",
-    "shipping_pincode": "",
-    "shipping_country": "",
-    "shipping_state": "",
-    "shipping_email": "",
-    "shipping_phone": "",
-    "order_items": [
-        {
-            "name": "Kunai",
-            "sku": "chakra123",
-            "units": 10,
-            "selling_price": "900",
-            "discount": "",
-            "tax": "",
-            "hsn": 441122
-        }
-    ],
-    "payment_method": "Prepaid",
-    "shipping_charges": 0,
-    "giftwrap_charges": 0,
-    "transaction_charges": 0,
-    "total_discount": 0,
-    "sub_total": 9000,
-    "length": 10,
-    "breadth": 15,
-    "height": 20,
-    "weight": 2.5
-};
-
 class ShiprocketController {
     async authToken(req: Request, res: Response) {
         logging.info(NAMESPACE, 'Entry authToken()');
@@ -83,14 +33,19 @@ class ShiprocketController {
 
     async createAdhocOrder(req: Request, res: Response) {
         const { token } = req?.query;
+        const payload = req.body as OrderPayload;
         try {
             if (!token)
-                return res.status(400).json({
+                return res.status(401).json({
                     msg: 'Authorization denied'
+                });
+            else if (!payload)
+                return res.status(400).json({
+                    msg: 'Bad request'
                 });
             else {
                 const shiprocketApi = new ShiprocketApi(token as string);
-                const orderResponse: any = await shiprocketApi.createOrder(orderPayload);
+                const orderResponse: OrderResponse = await shiprocketApi.createOrder(payload);
 
                 return res.status(201).json({
                     orderResponse
